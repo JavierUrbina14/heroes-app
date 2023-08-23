2748da5c6554cce0d96675aefd91e947dae62195
Javier Urbina <htorres332.jt@gmail.com>
2023-08-23 02:56:27 -0400

    fix image bug

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -17,7 +17,7 @@ export const HeroCard = ({
     characters ,
 }) => {
 
-    const heroImageUrl = `https://github.com/JavierUrbina14/heroes-app/tree/main/assets/heroes/${ id }.jpg`;
+    const heroImageUrl = `https://raw.githubusercontent.com/JavierUrbina14/heroes-app/main/assets/heroes/${id}.jpg`;
 
     return (
         <div className="col animate__animated animate__fadeIn">
